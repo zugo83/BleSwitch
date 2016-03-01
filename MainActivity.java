b2e08163b4a59f12601ec457323815090ee77e77
zugo83 <ste.ugolini@gmail.com>
2016-03-01 21:46:19 +0100

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.zugo.bleswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void buttonClick(View view) {
+
+        Log.d("MainActivity","Button pressed");
+
+
+    }
+}
